9695253fc7f0b7f922c609449b0411f4715657bd
Reza Fitriaman <rezafitriaman@pop-os.localdomain>
2021-09-23 15:26:15 +0200

    finished destop slider

--- src/index.ts
+++ src/index.ts
@@ -139,21 +139,25 @@ const checkBoundary = (diff: number) => {
 class Slider {
     trackContainer: HTMLElement;
     track: HTMLElement;
-    initialPosition: number;
+    initialPositionMouseX: number;
     isMoving: boolean;
-    trackTransformValue: number;
     marginLeftContainer: number;
-    goLeft: number;
+    transformValue: number;
+    currentPositionMouseXWhenMove: number
+    allPositionMouseXWhenMove: Array<number>;
+    //animationTiming: number;
     //travelDistanceFromInitialPosition: number;
 
     constructor() {
         this.trackContainer = document.querySelector('.slider-container')! as HTMLElement;
         this.track = document.querySelector('.slider-content-track')! as HTMLElement;
-        this.initialPosition = 0;
+        this.initialPositionMouseX = 0;
+        this.transformValue = 0;
         this.marginLeftContainer = this.getMarginLeftFromContainer;
-        this.trackTransformValue = this.getTrackTransformMatrix;
         this.isMoving = false;
-        this.goLeft = 0;
+        this.currentPositionMouseXWhenMove = 0;
+        this.allPositionMouseXWhenMove = [];
+        //this.animationTiming = 50;
         //this.travelDistanceFromInitialPosition = 0;
     }
 
@@ -175,52 +179,93 @@ class Slider {
         return pageX - this.marginLeftContainer;
     }
 
-    checkBoundary(travelDistanceFromInitialPosition:number) {
-        this.goLeft = this.trackTransformValue - travelDistanceFromInitialPosition;
+    checkBoundary(travelDistanceFromInitialPosition: number) {
+        let endOfLeftEdge = this.transformValue + travelDistanceFromInitialPosition > 0;
+        let endOfRightEdge = (this.transformValue + travelDistanceFromInitialPosition ) < -(this.track.scrollWidth - this.trackContainer.offsetWidth);
 
-        console.log('go left', this.goLeft);
+        if(endOfLeftEdge) {
+            this.track.style.transform = `transLateX(${0}px)`;
+        }else if(endOfRightEdge) {
+            this.track.style.transform = `transLateX(${-(this.track.scrollWidth - this.trackContainer.offsetWidth)}px)`;
+        } else {
+            this.track.style.transform = `transLateX(${this.transformValue + travelDistanceFromInitialPosition}px)`;
+        }
+    }
 
-        /*        if(this.trackTransformValue + travelDistanceFromInitialPosition > 0) {
+    direction(): string {
+        const lastPositionMouseXWhenMove = this.allPositionMouseXWhenMove.pop() as number;
+        const previousPositionMouseXWhenMove = this.allPositionMouseXWhenMove[this.allPositionMouseXWhenMove.length - 2]
 
-        }*/
+        return (lastPositionMouseXWhenMove < previousPositionMouseXWhenMove) ? 'right' : 'left';
     }
 
     mouseDown() {
         return (event: MouseEvent) => {
+            this.transformValue = this.getTrackTransformMatrix;
             this.isMoving = true;
-            this.initialPosition = this.getMousePosition(event.pageX);
-            this.track.style.transform = `transLateX(${this.trackTransformValue}px)`;
+            this.initialPositionMouseX = this.getMousePosition(event.pageX);
+            this.track.style.transform = `transLateX(${this.transformValue}px)`;
             this.track.classList.add('active');
-            //console.log('initialPosition', this.initialPosition);
+            console.log(' this.initialPosition', this.initialPositionMouseX);
         }
     }
 
     mouseMove() {
         return (event: MouseEvent) => {
             if(!this.isMoving) return;
-
-            let currentPosition = this.getMousePosition(event.pageX);
-            let travelDistanceFromInitialPosition = currentPosition - this.initialPosition;
-
+            this.currentPositionMouseXWhenMove = this.getMousePosition(event.pageX);
+            let travelDistanceFromInitialPosition = this.currentPositionMouseXWhenMove - this.initialPositionMouseX;
+            this.allPositionMouseXWhenMove.push(this.currentPositionMouseXWhenMove);
             this.checkBoundary(travelDistanceFromInitialPosition);
-            console.log('mousemove');
-            console.log('currentPosition', currentPosition);
-            console.log('initialPosition', this.initialPosition);
-            console.log('travelDistanceFromInitialPosition', travelDistanceFromInitialPosition);
         }
     }
 
     mouseUp() {
         return (event: MouseEvent) => {
             console.log('mouseUp');
-            console.log(event);
+            console.log('event', event);
+            const direction: string = this.direction();
+
+            console.log('getTrackTransformMatrix', this.getTrackTransformMatrix);
+            let timing = 0
+
+            let move = () =>{
+                timing = timing + 5;
+                console.log(timing);
+                console.log('direction', direction);
+
+                if(direction === 'left') {
+                    this.track.style.transform = `transLateX(${this.getTrackTransformMatrix + timing}px)`;
+
+                    if(this.getTrackTransformMatrix + timing > 0) {
+                        this.track.style.transform = `transLateX(${0}px)`;
+                    }
+                }
+
+                if(direction === 'right') {
+                    this.track.style.transform = `transLateX(${this.getTrackTransformMatrix + -timing}px)`;
+
+                    if((this.getTrackTransformMatrix + -timing) < -(this.track.scrollWidth - this.trackContainer.offsetWidth)) {
+                        this.track.style.transform = `transLateX(${-(this.track.scrollWidth - this.trackContainer.offsetWidth)}px)`;
+                    }
+                }
+
+
+                if(timing < 80) requestAnimationFrame(move);
+            }
+
+            requestAnimationFrame(move);
+
             this.isMoving = false;
+            this.track.classList.remove('active');
+
         }
     }
 
     mouseLeave() {
         return (event: MouseEvent) => {
             this.isMoving = false;
+            this.track.classList.remove('active');
             console.log('mouseLeave');
             console.log(event);
         }
@@ -230,7 +275,7 @@ class Slider {
         this.track.addEventListener('mousedown', this.mouseDown());
         this.track.addEventListener('mousemove', this.mouseMove());
         this.track.addEventListener('mouseup', this.mouseUp());
-        this.track.addEventListener('mouseleave', this.mouseLeave());
+        this.track.addEventListener('mouseleave', this.mouseUp());
     }
 }
 
